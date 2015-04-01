0e32f3597e5983db0c8f46996e0f6a89d4394e33
Tim Menzies <tjmenzie@ncsu.edu>
2015-04-01 12:03:32 -0400

    stuff

--- unittest.py
+++ unittest.py
@@ -9,32 +9,49 @@ https://www.youtube.com/watch?v=nIonZ6-4nuU.
 For example usage, see the _ok function (at end)
 """
 
+##-- top-level drivers ----------------------------
+def ok(*f):
+  """convenience function for unittests and 
+   running+testing code at load time."""
+  unittest(f);
+  return f[0] if listp(f) else f
+    
+class unittest:
+  "The worker."
+  tries = fails = 0  #  tracks the record so far
+  def __init__(i,*tests):
+    for test in items(tests):
+      unittest.tries += 1
+      try:
+        test()
+      except Exception,e:
+        unittest.fails += 1
+        print "# TRIES= %s FAIL= %s %s" % (
+              unittest.tries, unittest.fails,
+              "TEST= %s : %s" % (test.__name__,e))
+
+
+#---| misc support code |--------------------------
+def listp(x):
+  return isinstance(x,(list,tuple))
+
 def items(x):  
   """Convenience iterator. Recursively returns all
       non-list items within nested lists."""
-  if isinstance(x,(list,tuple)):
+  if listp(x):
     for y in x:
       for z in items(y):
         yield z
   else:
     yield x
-    
-class ok:
-  tries = fails = 0  #  tracks the record so far
-  def __init__(i,*tests):
-    for test in items(tests):
-      ok.tries += 1
-      try:
-        test()
-      except Exception,e:
-        ok.fails += 1
-        print "# TRIES= %s FAIL= %s %s" % (
-              ok.tries, ok.fails,
-              "TEST= %s : %s" % (test.__name__,e))
-        
+
+#---| example calls |------------------------------
+@ok # how to always run+test something at load time
 def noop():
   "Never fails"
   return True
+
+@ok # how to always run+test something at load time
 def oops():
   "Always fails"
   5/0
@@ -43,8 +60,9 @@ def _ok():
   "Test the test engine"
   ok(oops,noop,
      lambda: 1+1,lambda: 4/0)
-  assert ok.tries == 4
-  assert ok.fails == 2
+  assert unittest.tries == 6
+  assert unittest.fails == 3
 
+#---| maybe, test the test engine |---------------
 if __name__ == '__main__':
   _ok(); print "Success!!!"
