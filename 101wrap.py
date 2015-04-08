339335fde7636ec8676510cc1d059248f4f069e1
Tim Menzies <menzies.tim@gmail.com>
2015-04-08 14:47:47 +0000

    stuff

--- 101wrap.py
+++ 101wrap.py
@@ -20,12 +20,25 @@ def speed(f,repeats=10):
 
 ## Fibonacci (the Wrong Way)
 
-The following dobly-recursive version blows up after n> 32.
+The following doubly -recursive version blows up after n> 32.
 
 """
 def slowFib(n):
   return 1 if n<2 else slowFib(n-1) + slowFib(n-2)
 """
+
+Of course, we could be clever and carry round the n-1)
+result to use with the nth result:
+
+"""
+def fasterFib(n):
+    a, b = 0, 1
+    for i in xrange(n):
+        a, b = b, a + b
+    return a
+"""
+
+But wouldn't it be great if we didn't have to be clever?
 
 ## Wrapper magic
 
@@ -51,10 +64,16 @@ def fib(n):
 """
 def demo():
   for n in [10,20,30,32]:
-    print "slowFib %s %g secs" % (n, speed(lambda : slowFib(n)))
+    print "slowFib %s %g secs" % (
+      n, speed(lambda : slowFib(n)))
   print ""
   for n in [10,20,30,32,64,128,256,512]:
-    print "fastFib %s %g secs" % (n, speed(lambda : fib(n)))
+    print "fasterFib %s %g secs" % (
+      n, speed(lambda : fasterFib(n)))
+  print ""
+  for n in [10,20,30,32,64,128,256,512]:
+    print "fastFib %s %g secs" % (
+      n, speed(lambda : fib(n)))
 
 demo()
 """
@@ -63,22 +82,33 @@ demo()
 
 The slow way starts blowing up at 30. Warning. Do not run this for n=33
 
-  > python 101wrap.py
+    > python 101wrap.py
   
-  slowFib 10 4.56e-05 secs
-  slowFib 20 0.0033521 secs
-  slowFib 30 0.516437 secs
-  slowFib 32 1.49789 secs
-  
-The fast way (with memoing) just keeps on trucking.
+    slowFib 10 2.28e-05 secs
+    slowFib 20 0.002417 secs
+    slowFib 30 0.48794 secs
+    slowFib 32 1.31186 secs
+    
+The faster way (using clever programming) scales better.
 
-  fastFib 10 9.3e-06 secs
-  fastFib 20 7.4e-06 secs
-  fastFib 30 8e-06 secs
-  fastFib 32 5.4e-06 secs
-  fastFib 64 4.02e-05 secs
-  fastFib 128 2.8e-05 secs
-  fastFib 256 4.31e-05 secs
-  fastFib 512 9.69e-05 secs
+    fasterFib 10 6.3e-06 secs
+    fasterFib 20 6.8e-06 secs
+    fasterFib 30 7.7e-06 secs
+    fasterFib 32 7.4e-06 secs
+    fasterFib 64 8.9e-06 secs
+    fasterFib 128 1.65e-05 secs
+    fasterFib 256 2.53e-05 secs
+    fasterFib 512 4.63e-05 secs
   
+And the simpler way (with memoing) also works fine.
+    
+    fastFib 10 7.3e-06 secs
+    fastFib 20 5.4e-06 secs
+    fastFib 30 5.2e-06 secs
+    fastFib 32 2.4e-06 secs
+    fastFib 64 2.45e-05 secs
+    fastFib 128 1.39e-05 secs
+    fastFib 256 2.17e-05 secs
+    fastFib 512 8.18e-05 secs
+
 """
