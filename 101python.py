180e18e5bbe6f7c0cd3ad45e4a47ff5104534bb4
Tim Menzies <menzies.tim@gmail.com>
2015-04-10 01:41:18 +0000

    stuff

--- 101python.py
+++ 101python.py
@@ -2,6 +2,50 @@ from __future__ import print_function,division
 from unittest import *
 import math
 
+### lists
+
+@ok
+def _slice():
+  a=['a','b','c','d']
+  assert 'a'           == a[0]
+  assert ['b','c','d'] == a[1:]
+  assert ['a','b','c'] == a[:2]
+  assert 'd'           == a[-1]
+
+@ok     
+def _alias():
+  a    = [1, 2, 3]
+  b    = a
+  a[0] = 100
+  assert b[0] == 100
+
+@ok 
+def _clones():
+  a = [1, 2, 3]
+  b = a[:]
+  a[0]=100
+  assert b == [1,2,3] 
+
+def _dict():
+  inventory = {'apples': 430,
+               'bananas': 312, 'oranges': 525,
+               'pears': 217}
+  print("\n:all",inventory)
+  del inventory['pears']
+  print("\n:less",inventory)
+  print("\n:keys",inventory.keys())
+  print("\n:values",inventory.values())
+  print("\n:parts",inventory.items())
+  print("\n:iterate")
+  for key,value in inventory.items():
+    print(":key",key,":value",value)
+    
+def _counts():
+  seen = {}
+  for letter in "Mississippi":
+    seen[letter] = seen.get(letter, 0) + 1
+    print(seen)
+    
 class yourFirstClass:
   "init is called when created"
   def __init__(i,x) : i.mine = x
@@ -16,27 +60,23 @@ class Holds(dict):
   "Dictionary with a default value for unknown keys."
   def __init__(i, default):
     if callable(default):
-        print(1)
         i.default=default
-    else:
-        print(2)
+    else: 
         i.default = lambda :default
   def __getitem__(i, key):
     if key in i: 
       return i.get(key)
     else:
-      new=i.default()
-      print("new",new)
+      new=i.default() 
       return i.setdefault(key, new)   
 
 @ok
 def go0():
-  h = Holds(  1+2)
-  print(1+h["a"])
-  assert 1 == (1 + h["a"])
-  assert h["a"] == 0
-
-go0()
+  h = Holds( lambda : 1+2)
+  assert h["a"] + 2 == 5
+  h["b"] = 2
+  assert(h["b"] == 2) 
+   
 
 #################
 # your first function
@@ -74,10 +114,9 @@ def callingVars(*lst,**dic):
 def go4(): 
   assert callingVars(
           1,2,3,name='tim',age=54,shoesize=12) == (
-          [1,2,3], 
-          {'name':'tim','age':54,'shoesize':12}
-          )
-    
+          (1,2,3), 
+          {'age':54,'name':'tim','shoesize':12})
+
 ########
 
 def odd(x)      : return x % 2 == 1
@@ -95,7 +134,7 @@ def go5():
 def go6():
    lst = [8347, 6230, 923834, 1, 404, 993]
    out = [x for x in lst if x > 1000]
-   assert out == [6230, 8347, 923834]
+   assert out == [8347, 6230, 923834]
    
 def bigguns(lst,n):
   "defining inerators"
@@ -134,91 +173,37 @@ def go9():
   assert [key for key,_ in sorteddict(d)] == [
          'city','country','state','zip']
 
- 
 import string
   
 def _string2():
   "finds 'na' at '2' (i.e. third arg)"
   print(string.find("banana", "na"))
 
-
 def _string3():
   "as above, but starts at 3"
   print(string.find("banana", "na", 3))
 
-
 def _string4():
   "fails: specificed range has no 'na'"
   print(string.find("banana", "na", 2,3))
-
 
 def _whites():
   print("[" + string.whitespace+"]")
   for ch in string.whitespace:
     print(ord(ch),"["+ch+"]")
 
-
 def _lowers():
   print(string.lowercase)
-
 
 def isLower1(ch="a"):
   print(string.find(string.lowercase, ch) != -1)
 
-
 def isLower2(ch="b"):
   print(ch in string.lowercase)
-
 
 def isLower3(ch="c",banner="hekko there"):
   print(banner,'a' <= ch <= 'z')
 
-
 def _test1():
   isLower3(banner="work more on python",ch="D")
-
-
-def _slice():
-  a=[1,2,3,4]
-  print(0,a[0])
-  print(1,a[1:])
-  print(2,a[:2])
-  print(1,3,a[1:3])
-  print(-1,a[-1])
-    
-
-def _alias():
-  a = [1, 2, 3]
-  b = a
-  a[0]=100
-  print(a,b)
-  
-
-def _clones():
-  a = [1, 2, 3]
-  b = a[:]
-  a[0]=100
-  print(a,b)
-    
-def _dict():
-  inventory = {'apples': 430,
-               'bananas': 312, 'oranges': 525,
-               'pears': 217}
-  print("\n:all",inventory)
-  del inventory['pears']
-  print("\n:less",inventory)
-  print("\n:keys",inventory.keys())
-  print("\n:values",inventory.values())
-  print("\n:parts",inventory.items())
-  print("\n:iterate")
-  for key,value in inventory.items():
-    print(":key",key,":value",value)
-    
-
-def _counts():
-  seen = {}
-  for letter in "Mississippi":
-    seen[letter] = seen.get(letter, 0) + 1
-    print(seen)
-
 
