d8f8057e3dac8adc8d578de94840183837d94bf0
Tim Menzies <menzies.tim@gmail.com>
2015-04-11 14:48:29 +0000

    stuff

--- 101wrap.py
+++ 101wrap.py
@@ -75,7 +75,7 @@ def demo():
     print "fastFib %s %g secs" % (
       n, speed(lambda : fib(n)))
 
-demo()
+#demo()
 """
 
 ## Output
@@ -111,4 +111,141 @@ And the simpler way (with memoing) also works fine.
     fastFib 256 2.17e-05 secs
     fastFib 512 8.18e-05 secs
 
+## A More Advanced Example
+
+In any clustering system, the thing that
+takes the most time is working on the
+distance between rows in tables of
+data. 
+
 """
+def closest(row, rows):
+  "What row is closest within rows?" 
+  d, best = 10**32, None
+  for x in rows:
+    if x.id != row.id:
+      tmp = row - x # note: distance is "-"
+      if tmp < best:
+        d, best = tmp, x
+  return best
+"""
+
+In the above,  we define "i - x" to be
+the distance between two items.
+
+One trick for reducing this time is 
+to cache distance calcuations such that 
+if ever we calcuate i.dist(j) then 
+i AND j  remember that dist for 
+future reference. We'll call that
+"mirroring".
+
+Note that the following code cache's
+things in an entry marked with the name
+of the calling function. So this cache
+can cache things from many different functions.
+"""
+
+def cache(f):
+  "Caching, simple case, no mirroring."
+  what = lambda i: (f.__name__,i.id)
+  def wrapper(i):
+    if hasnot(i,"_cache"): i._cache = {}  
+    key = what(i)
+    if key in i._cache:
+      x = i._cache[key]
+    else:
+      x = f(i) # sigh, gonna have to call it
+    i._cache[key] =  x # ensure ache holds 'c'
+    return x
+  return wrapper
+  
+def cache2(f):
+  "Cache mirrored properties."
+  what = lambda i,j: (f.__name__,i.id, j.id)
+  def wrapper(i,j):
+    if hasnot(i,"_cache"): i._cache = {}  
+    if hasnot(j,"_cache"): j._cache = {}
+    if i.id > j.id: 
+      i,j = j,i # ids now sorted
+    key = what(i,j)  # what to cache
+    if key in i._cache:
+      x = i._cache[key]
+    elif key in j._cache:
+      x = j._cache[key]
+    else:
+      x = f(i,j) # sigh, gonna have to call it
+    i._cache[key] = j._cache[key] = x
+    return x
+  return wrapper
+
+"""
+
+One design decision here that is kinda fun
+is "where do we put the cache?". In the
+following, we create a Row object and keep
+the cache in the row. Then, if the row
+is ever garbage collected then "Zap!",
+its cache goes as well. Neat!
+
+"""
+def hasnot(inst,name):
+  "True if instance lacks variable."
+  return not name in inst.__dict__
+"""
+
+For that design decision to work, we'll
+have to refer to Rows by some unique numeric
+id (and not a pointer... why?) that is
+different for each row.
+
+"""
+class Row:
+  id=0
+  def __init__(i, cells=[]):
+    Row.id = i.id = Row.id + 1
+    i.cells = cells 
+  def __eq__(i,j) : return i.id == j.id
+  def __ne__(i,j) : return i.id != j.id
+  def value(i):
+    """Some slow domain specific value function
+    which we can sub-class for different rows."""
+    return slowFib(round((i[0] + i[-1]) / 2.0))
+  def __getitem__(i,key): 
+    "Impments row[i] (easy access to cells)."
+    return i.cells[key]
+  def __sub__(i,j):
+    "Implement i - j (the distance calc)."
+    return i.dist(j)
+  def __lt__(i,j):
+    "Implement i < j (used in sorting)."
+    return i.score() < j.score()
+  @cache # single argument cache
+  def score(i): 
+    return i.value()
+  @cache2 # doube argument symmetric caching
+  def dist(i,j):  
+    tmp = sum([(x -y)**2  for x,y in zip(i,j)])
+    return tmp**0.5 
+
+def rowDemo():
+  import random
+  random.seed(1) 
+  cellsPerRow    = 10 # cells perRow
+  maxRangeOfCell = 29
+  nRows          = 100
+  repeats        = 100
+  def row1():
+    "Make one row"
+    return Row([random.random()*maxRangeOfCell 
+                for _ in range(cellsPerRow)])
+  # make many rows
+  some = [row1() for _ in range(nRows)]
+  # tests the sorting code
+  sorted(some)  
+  # test the distance code
+  for _ in xrange(repeats):
+    for one in some:
+      one.nearest = closest(one, some)
+      
+rowDemo()
