91056f34c015b683654405c7ff588a228660ee34
Tim Menzies <tim@menzies.us>
2015-04-22 23:43:14 -0400

    saving

--- src/anywhere/table.py
+++ src/anywhere/table.py
@@ -1,4 +1,5 @@
 from lib import *
+from col import *
 
 @settings
 def TBL(): return o(
@@ -13,7 +14,7 @@ def TBL(): return o(
 def readcsv(file, t = None): 
   for cells in lines(file):
     if t:
-      Row(cells,t)
+      Row(t, cells)
     else:
       t = table0(cells)
   return t
@@ -34,7 +35,7 @@ def lines(file) :
         kept = ""
 
 def table0(cells):
-  t = o(num={},  sym={},  rows=[], all =[],
+  t = o(num={},  sym={},  rows=[], all =[],indep={},
         less={}, more={}, goal={}, fields=cells)
   my=the.TBL
   def nump(cell):
@@ -46,51 +47,46 @@ def table0(cells):
       hdr = t.num[i] = Num()  
     else:
       hdr = t.sym[i] = Sym()
-    hdr.txt  = cell
-    hdr.pos  = i
-    i.all += [hdr]
+    hdr.txt = cell
+    hdr.pos = i
+    t.all += [hdr]
     if my.less in cell: t.goal[i] = t.less[i] = hdr
     if my.more in cell: t.goal[i] = t.more[i] = hdr
     if not i in t.goal: t.indep[i]= hdr
-    i.all += [hdr]
   return t
 
-def clone(t) { return table0(t.fields) }
+def clone(t): return table0(t.fields) 
 
 class Row:
   id=0
-  def __init__(i,cells=[],t=None):
+  def __init__(i,t,cells=[]):
     Row.id  = i.id = Row.id + 1
     i.cells = cells
     i.cache = None
-    if t:
-      i.table = t
-      t.rows += [cells]
-      for hdr in t.all:
-        tmp = cell[hdr.i]
-        if tmp != the.TBL.skip:
-           hdr += cell[hdr.i]
+    i.table = t
+    t.rows += [cells]
+    for hdr in t.all:
+      tmp = cells[hdr.pos]
+      if tmp != the.TBL.skip:
+        hdr += tmp
   def __getitem__(i,k): return i.cells[k]
   def __hash__(i)     : return i.id
   def __repr__(i): return '<'+str(i.cells)+'>'
-
-  
-  #@memo2
-  def dist(i,j):
-    n = all = 0
-    for v1,v2,hdr in cells2(i,j,i.table.indep):
-      v1   = hdr.norm(v1)
-      v2   = hdr.norm(v2)
-      n   += 1
-      all += hdr.dist(v1,v2) 
-    return all**0.5 / (n+0.000001)**0.5
-    
-def cells2(row1,row2,headers):
-  skip = lambda z: z == the.TABLE.skip
-  for header in headers:
-    v1 = row1[header.col]
-    v2 = row2[header.col]
-    if not (skip(v1) and skip(v2)):
-      if skip(v1): v1 = hdr.far(v2)
-      if skip(v2): v2 = hdr.far(v1)
-      yield v1, v2, header
+  def __sub__(i,j): 
+    n = inc = 0
+    skip = the.TBL.skip
+    for hdr in i.table.indep.values():
+      k    = hdr.pos
+      x, y = i[k], j[k]
+      if x == y == skip:
+        continue
+      n += 1
+      if k in i.table.sym:
+        inc += 0 if x==y else 1
+      else:
+        if x != skip: x= hdr.norm(x)
+        if y != skip: y= hdr.norm(y)
+        if x == skip: x= 1 if y<0.5 else 0
+        if y == skip: y= 1 if x<0.5 else 0
+        inc  += (x-y)**2
+    return inc**0.5 / (n + 0.000001)**0.5
---
+++ src/anywhere/tableok.py
@@ -0,0 +1,8 @@
+from __future__ import print_function, division
+
+from table import *
+
+t=readcsv("weather.csv")
+
+print(len(t.rows))
+show(t.num)
