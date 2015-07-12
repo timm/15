c32ce477361dfa8b0fd2ea25441b26f2839df1c5
Tim Menzies <tim@menzies.us>
2015-07-11 23:48:06 -0400

    saving

--- web/etc/head.sh
+++ web/etc/head.sh
@@ -8,28 +8,30 @@ cat <<EOF
   <meta name="HandheldFriendly" content="True">
 
   <title>Tim Menzies: $head </title>
+
    <link rel="stylesheet"                               href="css/font-awesome.css">
   <link rel="stylesheet" type="text/css" media="screen" href="css/concise.css" />
   <link rel="stylesheet" type="text/css" media="screen" href="css/masthead.css" />
   <link rel="stylesheet" href="css/font-awesome.css">
   <link rel="stylesheet" href="css/mine.css">
+ <link rel="icon" href="img/favicon.png" type="image/png" />
 </head>
 <body>
   <header class="siteHeader container clearfix">
     <h1 class="logo">
       
         <a href="http://github.com/timm"><img class="bg1" src="icons/github10.png"></a>
-	<a href=""><img class="bg1" src="icons/facebook29.png"></a>
-	<a href=""><img class="bg1" src="icons/twitter12.png"></a>
-	<a href=""><img class="bg1" src="icons/slideshare3.png"></a>
-	<a href=""><img class="bg1" src="icons/storage27.png"></a>
-	<a href=""><img class="bg1" src="icons/linkedin12.png"></a>
+	<a href="https://www.facebook.com/timmenzies"><img class="bg1" src="icons/facebook29.png"></a>
+	<a href="https://twitter.com/timmenzies"><img class="bg1" src="icons/twitter12.png"></a>
+	<a href="http://slideshare.com/timmenzies"><img class="bg1" src="icons/slideshare3.png"></a>
+	<a href="https://www.dropbox.com/sh/2hkf4q271jg5t0o/AAAp9ggE1iMC5bgORnseJay7a?dl=0"><img class="bg1" src="icons/storage27.png"></a>
+	<a href="https://www.linkedin.com/profile/view?id=420532&trk=nav_responsive_tab_profile_pic"><img class="bg1" src="icons/linkedin12.png"></a>
     	</h1>
     <nav class="nav">
       <ul>
-        <li><a href="#about">Home</a></li>
-        <li><a href="">Papers</a></li>
-        <li><a href="">CV</a></li>
+        <li><a href="http://menzies.us">Home</a></li>
+        <li><a href="http://scholar.google.com/citations?hl=en&user=7htTUTgmLtUC&view_op=list_works&sortby=pubdate">Papers</a></li>
+        <li><a href="img/menziesCV.pdf">CV</a></li>
         <li><a href="http://ai4se.net">Visit my lab</a></li>
       </ul>
     </nav>
